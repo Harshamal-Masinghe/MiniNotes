1438183e50eef456fd15fa5e20b78d733a7581c6
Harsha <it22577306@my.sliit.lk>
2024-05-12 21:36:14 +0530

    define basic dao functions

---
+++ app/src/main/java/com/mininotes/database/NoteDao.kt
@@ -0,0 +1,29 @@
+package com.mininotes.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.mininotes.model.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM NOTES ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM NOTES WHERE noteTitle LIKE :query OR noteDesc LIKE :query")
+    fun searchNotes(query: String?): LiveData<List<Note>>
+
+}
